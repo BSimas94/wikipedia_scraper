7b9f987e0181ed2425b65a57241ff7925678257c
BSimas94 <blakesimas@gmail.com>
2018-08-07 19:42:32 -0700

    Cleaned code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pyodbc
 import wikipedia as wiki
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
 
 # CREATE THE CONNECTION
@@ -16,42 +16,19 @@ curs = cnxn.cursor()
 curs.execute("USE wikipedia")
 
 
-# SCRAPE 1000 RANDOM PAGES FROM WIKIPEDIA
-pages = wiki.random(pages=10)
-count = 0
-while count < 100:
-    for i in pages:
-        try:
-            curs.execute("INSERT INTO Page (Title, Summary) VALUES (?, ?)",
-            (wiki.WikipediaPage(title=i).title, wiki.WikipediaPage(title=i).summary))
-            html_page = wiki.WikipediaPage(title=i).html()
-            soup = BeautifulSoup(html_page)
-            links = soup.findAll('a', {'class': None})
-            curs.execute("UPDATE Page SET Link1Name = ? WHERE Title = ?",
-            (links[0].get('title'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link1URL = ? WHERE Title = ?",
-            ('https://en.wikipedia.org' + links[0].get('href'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link2Name = ? WHERE Title = ?",
-            (links[1].get('title'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link2URL = ? WHERE Title = ?",
-            ('https://en.wikipedia.org' + links[1].get('href'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link3Name = ? WHERE Title = ?",
-            (links[2].get('title'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link3URL = ? WHERE Title = ?",
-            ('https://en.wikipedia.org' + links[2].get('href'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link4Name = ? WHERE Title = ?",
-            (links[3].get('title'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link4URL = ? WHERE Title = ?",
-            ('https://en.wikipedia.org' + links[3].get('href'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link5Name = ? WHERE Title = ?",
-            (links[4].get('title'), wiki.WikipediaPage(title=i).title))
-            curs.execute("UPDATE Page SET Link5URL = ? WHERE Title = ?",
-            ('https://en.wikipedia.org' + links[4].get('href'), wiki.WikipediaPage(title=i).title))
-            cnxn.commit()
-        except:
-            pass
-    count += 1
-    pages = wiki.random(pages=10)
+# SCRAPE DATA FROM A SPECIFIED PAGE
+i = "Ex Machina (film)"
+html_page = wiki.WikipediaPage(title=i).html()
+soup = BeautifulSoup(html_page,features="html.parser")
+links = soup.findAll('a', {'class': None})
+curs.execute("INSERT INTO Page (Title, Summary, Link1Name, Link1URL, Link2Name, Link2URL, Link3Name, Link3URL, Link4Name, Link4URL, Link5Name, Link5URL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+wiki.WikipediaPage(title=i).title, wiki.WikipediaPage(title=i).summary,
+links[0].get('title'),'https://en.wikipedia.org' + links[0].get('href'),
+links[1].get('title'),'https://en.wikipedia.org' + links[1].get('href'),
+links[2].get('title'),'https://en.wikipedia.org' + links[2].get('href'),
+links[3].get('title'),'https://en.wikipedia.org' + links[3].get('href'),
+links[4].get('title'),'https://en.wikipedia.org' + links[4].get('href'))
+cnxn.commit()
 
 
 # READ DATA FROM DB
